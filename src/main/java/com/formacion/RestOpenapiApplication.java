9d9af28451fc5cf61ebeef67cb6f9ec0c5b8bfbd
jorgeferalv@gmail.com <jorgeferalv@gmail.com>
2023-05-08 10:37:56 +0200

    first commit

---
+++ src/main/java/com/formacion/RestOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.formacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOpenapiApplication.class, args);
+	}
+
+}
